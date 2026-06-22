ca77e476af21fd95aab2cf7938fd40a089e8f76b
TasminskyiRuslan <tasminskyi.ruslan@gmail.com>
2026-06-22 13:05:41 +0000

    Added swagger documentation for UnbanUserController

--- app/Http/Controllers/Api/User/UnbanUserController.php
+++ app/Http/Controllers/Api/User/UnbanUserController.php
@@ -8,11 +8,50 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
+use OpenApi\Attributes as OA;
 
 class UnbanUserController extends Controller
 {
     use AuthorizesRequests;
 
+    #[OA\Patch(
+        path: '/users/{user}/unban',
+        description: 'Unban the specified user.',
+        summary: 'Unban a user',
+        security: [['sanctum' => []]],
+        tags: ['User'],
+        parameters: [
+            new OA\Parameter(
+                name: 'user',
+                description: 'User identifier (slug).',
+                in: 'path',
+                required: true,
+                schema: new OA\Schema(
+                    type: 'string',
+                    example: 'john-doe'
+                )
+            )
+        ],
+        responses: [
+            new OA\Response(
+                response: SymfonyResponse::HTTP_NO_CONTENT,
+                description: 'User unbanned successfully.'
+            ),
+            new OA\Response(
+                response: SymfonyResponse::HTTP_UNAUTHORIZED,
+                description: 'User is unauthenticated.'
+            ),
+            new OA\Response(
+                response: SymfonyResponse::HTTP_FORBIDDEN,
+                description: 'User does not have permissions.'
+            ),
+            new OA\Response(
+                response: SymfonyResponse::HTTP_NOT_FOUND,
+                description: 'User not found.'
+            )
+        ]
+    )]
     /**
      * Unban the specified user.
      *
